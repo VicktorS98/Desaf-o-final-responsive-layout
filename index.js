7eb195baa0c141518fbb7dd5dfcf5446442ca820
VicktorS98 <100975523+VicktorS98@users.noreply.github.com>
2022-04-29 19:52:36 -0300

    initial upload

---
+++ index.js
@@ -0,0 +1,28 @@
+/* $("#nav-toggle").prop("checked", false);
+$("nav-toggle-label").prop("disabled", false); */
+
+
+/* se usaría este de abajo si tuviera display none al tocar el toggle*/
+
+/* $(document).ready(function () {
+    var toggle = $("#nav-toggle")
+    toggle.attr("checked", true);
+}); */
+
+/*lo de abajo era para probar nomás */
+
+
+/* $("#nav-toggle").click(function () {
+    $("nav").hide();
+}); */
+
+
+/* $("#nav-toggle").click(function(){
+    var toggle = $("#nav-toggle");
+    var nav = $("nav");
+    if (toggle.attr("checked", true)) {
+        nav.show();
+    };
+}); */
+
+
